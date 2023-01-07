d8f60ac515a188914df0a07807c443994c3a9249
Philip Welin-Berger <philip.welin.berger@gmail.com>
2023-01-07 10:43:58 +0900

    added logic to store textures and the texture for tiles

---
+++ src/texture_storage.rs
@@ -0,0 +1,33 @@
+use std::collections::HashMap;
+extern crate sfml;
+use sfml::{graphics::Texture, SfBox};
+
+pub struct TextureStorage {
+    texture_map: HashMap<TextureIdentifiers, SfBox<Texture>>,
+}
+
+impl TextureStorage {
+    pub fn new() -> TextureStorage {
+        TextureStorage {
+            texture_map: HashMap::new(),
+        }
+    }
+
+    pub fn load(&mut self, identifier: TextureIdentifiers, filename: &str) {
+        // TODO: PROPERLY HANDLE THE UNWRAP CALL LMAO
+        let texture = Texture::from_file(filename).unwrap();
+        self.texture_map.insert(identifier, texture);
+    }
+
+    pub fn get(&self, identifier: TextureIdentifiers) -> &SfBox<Texture> {
+        //TODO: PROPERLY HANDLE THE UNWRAP CALL LMAO
+        self.texture_map.get(&identifier).unwrap()
+    }
+}
+
+#[derive(PartialEq, Eq, Hash)]
+pub enum TextureIdentifiers {
+    Nebula,
+    Rocket,
+    Tile,
+}
