6237bc3aeffbeb909d7a0ce50456174e55e1b366
Philip Welin-Berger <philip.welin.berger@gmail.com>
2023-01-17 15:11:15 +0900

    chagned name to CardinalDirection & removed debug prints

--- src/lib.rs
+++ src/lib.rs
@@ -2,33 +2,40 @@ use sfml::system::Vector2;
 use strum_macros::EnumIter;
 
 #[derive(Debug, EnumIter, Copy, Clone)]
-pub enum CardinalDirections {
+pub enum CardinalDirection {
     Up,
     Down,
     Left,
     Right,
 }
 
-impl CardinalDirections {
+impl CardinalDirection {
     pub fn get_direction_coordinates(&self, coordinates: Vector2<usize>) -> Vector2<usize> {
-        println!("{:?}", coordinates);
         match self {
-            CardinalDirections::Up => Vector2 {
+            CardinalDirection::Up => Vector2 {
                 x: coordinates.x,
                 y: std::cmp::max(coordinates.y, 1) - 1,
             },
-            CardinalDirections::Down => Vector2 {
+            CardinalDirection::Down => Vector2 {
                 x: coordinates.x,
                 y: coordinates.y + 1,
             },
-            CardinalDirections::Right => Vector2 {
+            CardinalDirection::Right => Vector2 {
                 x: coordinates.x + 1,
                 y: coordinates.y,
             },
-            CardinalDirections::Left => Vector2 {
+            CardinalDirection::Left => Vector2 {
                 x: std::cmp::max(coordinates.x, 1) - 1,
                 y: coordinates.y,
             },
+        }
+    }
+    pub fn get_opposite_direction(&self) -> CardinalDirection {
+        match self {
+            CardinalDirection::Up => CardinalDirection::Down,
+            CardinalDirection::Down => CardinalDirection::Up,
+            CardinalDirection::Right => CardinalDirection::Left,
+            CardinalDirection::Left => CardinalDirection::Right,
         }
     }
 }
