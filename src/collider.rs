29c2f955826a317c54b6fe3eb0c1e4b44c81df9f
Philip Welin-Berger <philip.welin.berger@gmail.com>
2023-03-01 11:35:59 +0900

    fixed bug where intersect didnt use abs of delta

--- src/collider.rs
+++ src/collider.rs
@@ -1,3 +1,5 @@
+use std::os::unix::process;
+
 use sfml::system::Vector2f;
 
 #[derive(Debug)]
@@ -17,26 +19,38 @@ impl Collider {
         let self_position: Vector2f = self.get_position();
         let self_half_size: Vector2f = self.get_half_size();
 
+        println!("player pos is: {self_position:?}");
+        println!("other pos is: {other_position:?}");
         let delta = other_position - self_position;
-        let intersect = delta - (other_half_size + self_half_size);
+        println!("Delta is: {delta:?}");
+        let intersect = Vector2f::new(
+            delta.x.abs() - (other_half_size.x + self_half_size.x),
+            delta.y.abs() - (other_half_size.y + self_half_size.y),
+        );
 
+        if self.position.y.is_nan() {
+            panic!("y became undefined...");
+        }
         if intersect.x < 0.0 && intersect.y < 0.0 {
             push = push.max(0.0).min(1.0);
-
             if intersect.x > intersect.y {
                 if delta.x > 0.0 {
                     self.update(intersect.x * (1.0 - push), 0.0);
                     other.update(-intersect.x * push, 0.0);
+                    println!("x > 0: other pos = {other_position:?}");
                 } else {
                     self.update(-intersect.x * (1.0 - push), 0.0);
                     other.update(intersect.x * push, 0.0);
+                    println!("else x > 0: other pos = {other_position:?}");
                 }
             } else if delta.y > 0.0 {
                 self.update(0.0, intersect.y * (1.0 - push));
                 other.update(0.0, -intersect.y * push);
+                println!("y > 0: other pos = {other_position:?}");
             } else {
                 self.update(0.0, -intersect.y * (1.0 - push));
                 other.update(0.0, intersect.y * push);
+                println!("else y > 0: other pos = {other_position:?}");
             }
             return true;
         }
